565427a29c03a575f84f03730d70ba7f218ee739
davidzuma <zumaquerodavid@gmail.com>
2022-12-18 20:15:01 +0100

    feat(puzzle_4): include part 2

--- puzzle_4/main.py
+++ puzzle_4/main.py
@@ -5,16 +5,20 @@ section_assignment_pairs: list = section_assignment_pairs.read()
 section_assignment_pairs = section_assignment_pairs.split("\n")
 
 
-def get_number_of_useless_pairs(section_assignment_pairs: List[str]) -> int:
+def get_number_of_useless_pairs(section_assignment_pairs: List[str]) -> Tuple[int]:
     number_of_useless_pairs = 0
+    number_of_overlapped_pairs = 0
+
     for pair in section_assignment_pairs:
         section_1, section_2 = get_sections(pair)
+        if section_1 & section_2 != set():
+            number_of_overlapped_pairs += 1
         if section_1.issubset(section_2) or section_2.issubset(section_1):
             number_of_useless_pairs += 1
-    return number_of_useless_pairs
+    return number_of_useless_pairs, number_of_overlapped_pairs
 
 
-def get_sections(pair: List[str]):
+def get_sections(pair: List[str]) -> List[set]:
     range_2 = lambda a, b: range(a, b + 1)
     sections = [
         set(range_2(*[int(pos) for pos in section.split("-")]))
